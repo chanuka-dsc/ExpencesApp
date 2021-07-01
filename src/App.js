191ee6c1a340304c680ba112225d2584babf0f60
chanuka-dsc <chanuka.dsc@gmail.com>
2021-07-01 19:19:48 +0530

    day one end

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import ExpenseItem from './Components/ExpenseItem'
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>This is also visible</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,13 @@
+const ExpenseItem = function () {
+    return (
+        <div class = "expense-item">
+            <div class ="date">date</div>
+            <div>
+                <h2 class = "title">title</h2>
+                <div class = "amount">anount</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem ; 
